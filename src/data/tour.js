51b0a58f41ae0ba8ccb2d900622c7068985556d3
vasudevshetty <vasudeepu2815@gmail.com>
2025-05-30 20:05:56 +0530

    feat: add new images and update tour steps descriptions in tour.js

--- src/data/tour.js
+++ src/data/tour.js
@@ -1,17 +1,17 @@
 export const tourSteps = [
   {
     name: "SJCE Entrance ",
-    description: "Start your tour at the iconic Pot Circle.",
+    description: "Start your tour at the iconic Pot Circle here.",
     shortDescription: "Tour starting point.",
-    images: ["/images/pot_circle.jpg"],
+    images: ["/images/SJCE Entrance.jpg"],
     coordinate: [76.615118, 12.313092],
   },
   {
     name: "Pot Circle, Main Entrance",
-    description: "Start your tour at the iconic Pot Circle.",
+    description: "Start your tour at the iconic Pot Circle here.",
     shortDescription:
       "Please move towards the registration desk to complete   the registration ",
-    images: ["/images/pot_circle.jpg"],
+    images: ["/images/Pot Circle, Main Entrance.jpg"],
     coordinate: [76.614141, 12.313176],
   },
   {
@@ -19,7 +19,7 @@ export const tourSteps = [
     description: "Get your registration done here.",
     shortDescription:
       "Registration help desk. Student volunteers will assist you in the registration process.",
-    images: ["/images/registration.jpg"],
+    images: ["/images/Registration Desk.jpg"],
     coordinate: [76.613826, 12.312986],
   },
   {
@@ -27,32 +27,34 @@ export const tourSteps = [
     description: "Get your admission queries answered here.",
     shortDescription:
       "Admission query desk. Staff available for all your admission and document queries. Assistance for new students provided here.",
-    images: ["/images/registration.jpg"],
+    images: ["/images/Admission Queries.jpg"],
     coordinate: [76.613686, 12.312996],
   },
   {
     name: "Electronics and Communication Department",
     description:
-      "Visit the electronics and communication department for the latest gadgets.",
+      "Visit the electronics and communication department for the latest gadgets here.",
     shortDescription:
       "Explore electronics innovations. See student projects and the latest gadgets in action. Hands-on demos available.",
-    images: ["/images/electronics.jpg"],
+    images: ["/images/Electronics and Communication Department.jpg"],
     coordinate: [76.613154, 12.312955],
   },
   {
     name: "Electrical and Electronics Engineering Department",
-    description: "Explore the electrical and electronics department.",
+    description:
+      "Explore the electrical and electronics engineering  department here.",
     shortDescription:
       "Electrical & electronics labs. Discover circuit design and automation. Faculty available for Q&A.",
-    images: ["/images/electrical.jpg"],
+    images: ["/images/Electrical and Electronics Engineering Department.jpg"],
     coordinate: [76.612891, 12.313147],
   },
   {
     name: "Mechanical Engineering Department",
-    description: "Discover innovations in the mechanical department.",
+    description:
+      "Discover innovations in the mechanical engineering  department here.",
     shortDescription:
       "Mechanical engineering zone. Explore robotics and machine design labs. Interactive displays for visitors.",
-    images: ["/images/mechanical.jpg"],
+    images: ["/images/Mechanical Engineering Department.jpg"],
     coordinate: [76.612811, 12.313532],
   },
   {
@@ -61,120 +63,132 @@ export const tourSteps = [
       "Visit Physics, chemistry, Mathametics and Biotechnology Departments here",
     shortDescription:
       " Physics, chemistry, Math and Biotechnology Departments are here to present their department activities.",
-    images: ["/images/auditorium.jpg"],
+    images: ["/images/Auditorium.jpg"],
     coordinate: [76.612784, 12.314249],
   },
   {
     name: "Construction Technology and Managment Department",
-    description: "Learn about construction technology.",
+    description: "Learn about construction technology and managment here.",
     shortDescription:
       "Construction Technology. Learn about various new method and techniques used in construction. View student models and ongoing research.",
-    images: ["/images/civil.jpg"],
+    images: ["/images/Construction Technology and Managment Department.jpg"],
     coordinate: [76.612988, 12.31424],
   },
   {
     name: "Civil Engineering Department",
-    description: "Learn about civil engineering.",
+    description: "Learn about civil engineering here.",
     shortDescription:
       "Civil engineering block. Learn about sustainable construction. View student models and ongoing research.",
-    images: ["/images/civil.jpg"],
+    images: ["/images/Civil Engineering Department.jpg"],
     coordinate: [76.613136, 12.314202],
   },
   {
     name: "Environmental Engineering Department",
-    description: "Explore environmental studies and sustainability.",
+    description: "Explore environmental studies and sustainability here.",
     shortDescription:
       "Environmental research center. Explore green technologies and sustainability projects. Meet the research team.",
-    images: ["/images/environmental.jpg"],
+    images: ["/images/Environmental Engineering Department.jpg"],
     coordinate: [76.613392, 12.314589],
   },
   {
     name: "Industrial Production Department",
-    description: "Learn about industrial production processes.",
+    description: "Learn about industrial production processes here.",
     shortDescription:
       "Industrial production hub. See manufacturing processes in action. Learn about industry partnerships.",
-    images: ["/images/industrial.jpg"],
+    images: ["/images/Industrial Production Department.jpg"],
     coordinate: [76.613543, 12.314906],
   },
   {
     name: "Polymer Science and Technology Department",
-    description: "Discover advancements in polymer science.",
+    description: "Discover advancements in polymer science here.",
     shortDescription:
       "Polymer science block. Discover new materials and their applications. Lab tours available on request.",
-    images: ["/images/polymer.jpg"],
+    images: ["/images/Polymer Science and Technology Department.jpg"],
     coordinate: [76.613575, 12.315231],
   },
   {
     name: "SJCE-STEP",
-    description: "Visit the SJCE-STEP for entrepreneurship support.",
+    description: "Visit the SJCE-STEP for entrepreneurship support here.",
     shortDescription:
       "Entrepreneurship centre. Support for student startups and innovation. Attend a workshop or mentoring session.",
-    images: ["/images/step.jpg"],
+    images: ["/images/SJCE-STEP.jpg"],
     coordinate: [76.613282, 12.31561],
   },
   {
     name: "Student Clubs",
-    description: "Visit the various Student clubs of our college",
+    description: "Visit the various Student clubs of our college here",
     shortDescription:
       "Student Clubs, being the integral part of students life. Visit the various student club to know more .",
-    images: ["/images/step.jpg"],
+    images: ["/images/Student Clubs.jpg"],
     coordinate: [76.613432, 12.31623],
   },
   {
+    name: "Golden Jubilee Bhavan",
+    description: "Now you are entering the Golden Jubilee Bhavan here.",
+    shortDescription: "Golden jubliee bhavan is were the universty resides .",
+    images: ["/images/Golden Jubilee Bhavan.jpg"],
+    coordinate: [76.613878, 12.316312],
+  },
+  {
     name: "Master of Computer Applications",
-    description: "Visit the Master of Computer Application ",
+    description: "Visit the Master of Computer Applications here",
     shortDescription:
       "Golden Jubilee Hall. Venue for special ceremonies and alumni meets. Explore the photo gallery inside.",
-    images: ["/images/golden_jubilee.jpg"],
+    images: ["/images/Master of Computer Applications.jpg"],
     coordinate: [76.614031, 12.31638],
   },
   {
     name: "Computer Science and Engineering Department",
-    description: "Visit the Computer Science and Engineering Department ",
+    description: "Visit the Computer Science and Engineering Department here ",
     shortDescription:
       "CSE department block. Cutting-edge research in AI and software. Visit the coding lab and interact with students.",
-    images: ["/images/cse.jpg"],
+    images: ["/images/Computer Science and Engineering Department.jpg"],
     coordinate: [76.614081, 12.316412],
   },
   {
     name: "Placement and Training Cell",
-    description: " visit the Placement and Training.",
+    description: " visit the Placement and Training here.",
     shortDescription:
       "In order to know the placement and training to know about the placement.",
-    images: ["/images/computer_applications.jpg"],
+    images: ["/images/Placement and Training Cell.jpg"],
     coordinate: [76.614161, 12.31633],
   },
   {
     name: "Management Studies ",
-    description: "Visit the Management Studies block.",
+    description: "Visit the Management Studies block here.",
     shortDescription:
       "To know more about management studies, visit our management studies desk.",
-    images: ["/images/computer_applications.jpg"],
+    images: ["/images/Management Studies.jpg"],
     coordinate: [76.614055, 12.316261],
   },
   {
     name: "Electronics and Instrumentation Engineering Department ",
     description:
-      "Visit the Electronics and Instrumentation Engineering department.",
+      "Visit the Electronics and Instrumentation Engineering department here.",
     shortDescription:
       " To observe cool hardware project, visit the Electronics and Instrumentation Engineering Department",
-    images: ["/images/computer_applications.jpg"],
+    images: [
+      "/images/Electronics and Instrumentation Engineering Department.jpg",
+    ],
     coordinate: [76.614061, 12.316249],
   },
   {
-    name: "Information Science and Engineering / CSBS Department",
-    description: "Visit the Information Science and Engineering Department.",
+    name: "Information Science and Engineering and CSBS Department",
+    description:
+      "Visit the Information Science and Engineering Department here.",
     shortDescription:
       "Information science block. Data science and analytics hub. Explore the latest in information technology.",
-    images: ["/images/ise.jpg"],
+    images: [
+      "/images/Information Science and Engineering and CSBS Department.jpg",
+    ],
     coordinate: [76.61417, 12.31627],
   },
   {
     name: "Physical Education Department",
-    description: "Visit the Physical Education Department.",
+    description: "Visit the Physical Education Department here.",
     shortDescription:
       "To know about the various activities related to physical fitness, health, and overall well-being ",
-    images: ["/images/computer_applications.jpg"],
+    images: ["/images/Physical Education Department.jpg"],
     coordinate: [76.614035, 12.316073],
   },
   {
@@ -182,7 +196,7 @@ export const tourSteps = [
     description: "Here Ends Our trip.",
     shortDescription:
       "Our trip ends here, hope you all enjoyed our campus trip.",
-    images: ["/images/computer_applications.jpg"],
+    images: ["/images/Exit.jpg"],
     coordinate: [76.613913, 12.315941],
   },
 ];
